b25e3dbca3d63d123453ee61caf02a7bd9d7adf6
Scott DeLuzio <scott@surpriseazwebservices.com>
2017-11-21 15:54:56 -0700

    Add additional fields to webhook data

--- add-to-webhook.php
+++ add-to-webhook.php
@@ -42,6 +42,37 @@ function filter_woocommerce_webhook_payload__conditional_checkout_fields( $paylo
 			}
 		}
 	}
+	foreach( $order->get_items() as $item_id => $item ){
+		$product_id	= $item->get_product_id();
+		$term_list	= wp_get_post_terms( $product_id, 'product_cat', array( 'fields'=>'ids' ) );
+		$cat_id		= (int)$term_list[0];
+		$category	= '';
+		foreach ( $term_list as $term ) {
+			$cat		= get_term( $term, 'product_cat' );
+			$category 	.= $cat->name . ',';
+		}
+		$category	= rtrim( $category, ',' );
+		$type		= $item->get_type();
+
+		$data		= array(
+			'id'			=>	$item_id,
+			'subtotal'		=>	wc_format_decimal( $order->get_line_subtotal( $item ), 2 ),
+			'total'			=>	wc_format_decimal( $order->get_line_total( $item ), 2 ),
+			'total_tax'		=>	wc_format_decimal( $order->get_line_tax( $item ), 2 ),
+			'price'			=>	wc_format_decimal( $order->get_item_total( $item ), 2 ),
+			'quantity'		=>	$item->get_quantity(),
+			'tax_class'		=>	$item->get_tax_class(),
+			'name'			=>	$item->get_name(),
+			'category'		=>	$category,
+			'product_id'	=>	$item->get_variation_id() ? $item->get_variation_id() : $item->get_product_id(),
+			'sku'			=>	is_object( $product ) ? $product->get_sku() : null,
+		);
+
+		$payload[$type][]	= $data;
+	}
+
+	$payload['date_created_formatted'] = date( get_option( 'date_format' ) . ' ' . get_option( 'time_format' ), $order->get_date_created()->getTimestamp() );
+
 
 	return $payload;
 };
